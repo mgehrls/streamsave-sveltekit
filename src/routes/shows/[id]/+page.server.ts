a81561043c2ee04da77c7989727f606616fb34c4
Matt Gehrls <mattgehrls@gmail.com>
2022-12-20 15:53:46 -0500

    added placeholders

--- src/routes/shows/[id]/+page.server.ts
+++ src/routes/shows/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from "./$types";
-import { API_KEY_SECRET } from "../../../lib/server/secrets";
+import { API_KEY_SECRET } from "$env/static/private";
 
 export const load = (async ({params}) =>{
     const res = fetch(`https://api.themoviedb.org/3/tv/${params.id}?api_key=${API_KEY_SECRET}&language=en-US`)
