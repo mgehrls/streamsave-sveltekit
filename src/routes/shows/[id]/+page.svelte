3509b50cdeab510fe032aa4c641bf1984f295c14
Matt Gehrls <mattgehrls@gmail.com>
2022-12-20 15:39:31 -0500

    got rid of movies for simplicity short term

--- src/routes/shows/[id]/+page.svelte
+++ src/routes/shows/[id]/+page.svelte
@@ -17,15 +17,38 @@
     src={`https://image.tmdb.org/t/p/w342/${data.item.poster_path}`}
     alt="show poster"
   />
-  <h1>{data.item.name}</h1>
-  <p>{data.item.overview}</p>
+  <div class="infosec">
+    <h1>{data.item.name}</h1>
+    <p>{data.item.overview}</p>
+    <div class="buttonsec">
+      <button>Does nothing</button>
+      <button>Placeholder</button>
+    </div>
+  </div>
 </section>
 
 <style>
   section {
     margin-top: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+  }
+  div.infosec {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 1rem;
+  }
+  div.buttonsec {
+    display: flex;
+    gap: 1rem;
   }
   h1 {
     color: black;
+    align-self: start;
+    margin: 0;
   }
 </style>
