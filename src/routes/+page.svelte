943c691a5a13df82dd92d65b3f2f6378075ed236
Matt Gehrls <mattgehrls@gmail.com>
2023-02-01 10:03:13 -0500

    test three

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,10 @@
 <svelte:head>
   <title>Home</title>
   <meta name="description" content="Svelte demo app" />
-  <meta name="viewport" content="width=device-width, user-scalable=no" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+  />
 </svelte:head>
 <svelte:window bind:innerWidth />
 
