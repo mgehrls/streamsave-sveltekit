b067bbb1cc065f145a5249dbb348cad473c6e2d9
Matt Gehrls <mattgehrls@gmail.com>
2023-02-02 11:10:54 -0500

    mobile friendly individual media pages

--- src/routes/movies/[id]/+page.svelte
+++ src/routes/movies/[id]/+page.svelte
@@ -1,53 +1,94 @@
 <script lang="ts">
+  import AddRemoveBtn from "$lib/AddRemoveBtn.svelte";
   import type { PageData } from "./$types";
+  import {
+    addListItem,
+    deleteListItem,
+    listItems,
+  } from "$lib/stores/listItems";
   export let data: PageData;
   const { item: media } = data;
+
+  let loading: boolean = false;
+  let innerWidth: number;
+  $: listItemsArray = $listItems;
+  $: if ($listItems) {
+    loading = false;
+  }
+
+  async function handleDelete() {
+    loading = true;
+    await deleteListItem(media.id);
+  }
+  async function handleAdd() {
+    loading = true;
+    await addListItem(
+      {
+        id: media.id,
+        title: media.title ? media.title : media.name,
+        description: media.overview,
+        type: media.title ? "movie" : "show",
+        backdrop_path: `https://image.tmdb.org/t/p/w342${media.backdrop_path}`,
+        poster_path: `https://image.tmdb.org/t/p/w342${media.poster_path}`,
+      },
+      data.session.user.id
+    );
+  }
 </script>
 
 <svelte:head>
-  <title>Movies</title>
-  <meta
-    name="description"
-    content="Details about a specific movies based on id"
-  />
+  <title>{"StreamSave: " + media.title}</title>
+  <meta name="description" content={`Details about ${media.title}`} />
 </svelte:head>
+<svelte:window bind:innerWidth />
 
-<section class="mt-6 mb-24 w-screen flex justify-center items-center">
+<section class="mt-8 w-screen flex justify-center items-center">
   <div
-    class="flex flex-col md:flex-row justify-center items-center gap-8 max-w-5xl"
+    class={innerWidth < 490
+      ? "max-w-5xl flex flex-col justify-center items-center gap-8 px-4"
+      : "max-w-5xl flex justify-center items-center gap-8 px-4"}
   >
-    <img
-      class="px-4"
-      src={`https://image.tmdb.org/t/p/w342${media.backdrop_path}`}
-      alt={`${media.title} poster`}
-    />
-    <div
-      class="flex flex-col justify-start items-start gap-4 w-full px-4 md:w-1/2"
-    >
-      <h2 class="m-0 font-bold text-lg text-slate-100 self-start">
-        {media.title}
-      </h2>
-      <div class="flex gap-4 justify-center items-center">
+    <div class="relative">
+      <img
+        src={`https://image.tmdb.org/t/p/w342${media.poster_path}`}
+        alt={`${media.title} poster`}
+      />
+      <div class="absolute top-4 right-4">
+        <AddRemoveBtn
+          {loading}
+          onList={listItemsArray.map((item) => item.media_id).includes(media.id)
+            ? true
+            : false}
+          {handleAdd}
+          {handleDelete}
+        />
+      </div>
+      <div class="flex mt-4 gap-4 justify-center items-center">
         <a
-          class="px-4 py-2 bg-slate-600 text-slate-100 rounded-md hover:bg-slate-300 hover:text-slate-900"
+          class="border border-slate-300 border-solid px-4 py-1 text-slate-100"
           rel="noreferrer"
           target="_blank"
           href={`https://www.imdb.com/title/${media.imdb_id}/`}>IMDB</a
         >
         <a
-          class="px-4 py-2 bg-slate-600 text-slate-100 rounded-md hover:bg-slate-300 hover:text-slate-900"
+          class="border border-slate-300 border-solid px-4 py-1 text-slate-100"
           rel="noreferrer"
           target="_blank"
           href={`${media.homepage}`}>Website</a
         >
       </div>
+    </div>
+    <div
+      class={innerWidth < 490
+        ? "flex flex-col justify-start items-start gap-4 bg-slate-500 p-4"
+        : "flex flex-col justify-start items-start gap-4 bg-slate-500 p-4 w-1/2 max-h-full"}
+    >
+      <h1 class="m-0 p-0 text-lg font-semibold text-slate-100 self-start">
+        {media.title}
+      </h1>
+      <p class="m-0 p-0 text-slate-100 italic opacity-90">"{media.tagline}"</p>
+
       <p class="text-slate-100">{data.item.overview}</p>
-      <div class="flex gap-4">
-        <button
-          class="px-4 py-2 bg-slate-600 text-slate-100 rounded-md hover:bg-slate-300 hover:text-slate-900"
-          >make add button work here.</button
-        >
-      </div>
     </div>
   </div>
 </section>
--- src/routes/shows/[id]/+page.svelte
+++ src/routes/shows/[id]/+page.svelte
@@ -1,45 +1,94 @@
 <script lang="ts">
+  import AddRemoveBtn from "$lib/AddRemoveBtn.svelte";
   import type { PageData } from "./$types";
+  import {
+    addListItem,
+    deleteListItem,
+    listItems,
+  } from "$lib/stores/listItems";
   export let data: PageData;
   const { item: media } = data;
+
+  let loading: boolean = false;
+  let innerWidth: number;
+  $: listItemsArray = $listItems;
+  $: if ($listItems) {
+    loading = false;
+  }
+
+  async function handleDelete() {
+    loading = true;
+    await deleteListItem(media.id);
+  }
+  async function handleAdd() {
+    loading = true;
+    await addListItem(
+      {
+        id: media.id,
+        title: media.title ? media.title : media.name,
+        description: media.overview,
+        type: media.title ? "movie" : "show",
+        backdrop_path: `https://image.tmdb.org/t/p/w342${media.backdrop_path}`,
+        poster_path: `https://image.tmdb.org/t/p/w342${media.poster_path}`,
+      },
+      data.session.user.id
+    );
+  }
 </script>
 
 <svelte:head>
-  <title>Movies</title>
-  <meta
-    name="description"
-    content="Details about a specific movies based on id"
-  />
+  <title>{"StreamSave: " + media.name}</title>
+  <meta name="description" content={`Details about ${media.name}`} />
 </svelte:head>
+<svelte:window bind:innerWidth />
 
 <section class="mt-8 w-screen flex justify-center items-center">
-  <div class="max-w-5xl flex justify-center items-center gap-8">
-    <img
-      src={`https://image.tmdb.org/t/p/w342${media.poster_path}`}
-      alt={`${media.title} poster`}
-    />
-    <div class="flex flex-col justify-start items-start gap-4 w-1/2">
-      <h1 class="m-0 p-0 text-slate-100 self-start">{media.name}</h1>
-      <p class="m-0 p-0 italic opacity-90">"{media.tagline}"</p>
-      <div class="flex gap-4 justify-center items-center">
+  <div
+    class={innerWidth < 490
+      ? "max-w-5xl flex flex-col justify-center items-center gap-8 px-4"
+      : "max-w-5xl flex justify-center items-center gap-8 px-4"}
+  >
+    <div class="relative">
+      <img
+        src={`https://image.tmdb.org/t/p/w342${media.poster_path}`}
+        alt={`${media.title} poster`}
+      />
+      <div class="absolute top-4 right-4">
+        <AddRemoveBtn
+          {loading}
+          onList={listItemsArray.map((item) => item.media_id).includes(media.id)
+            ? true
+            : false}
+          {handleAdd}
+          {handleDelete}
+        />
+      </div>
+      <div class="flex mt-4 gap-4 justify-center items-center">
         <a
-          class="border border-blue-700 border-solid pr-8"
+          class="border border-slate-300 border-solid px-4 py-1 text-slate-100"
           rel="noreferrer"
           target="_blank"
           href={`https://www.imdb.com/title/${media.imdb_id}/`}>IMDB</a
         >
         <a
-          class="border border-blue-700 border-solid pr-8"
+          class="border border-slate-300 border-solid px-4 py-1 text-slate-100"
           rel="noreferrer"
           target="_blank"
           href={`${media.homepage}`}>Website</a
         >
       </div>
+    </div>
+    <div
+      class={innerWidth < 490
+        ? "flex flex-col justify-start items-start gap-4 bg-slate-500 p-4"
+        : "flex flex-col justify-start items-start gap-4 bg-slate-500 p-4 w-1/2 max-h-full"}
+    >
+      <h1 class="m-0 p-0 text-lg font-semibold text-slate-100 self-start">
+        {media.name}
+      </h1>
+      <p class="m-0 p-0 text-slate-100 italic opacity-90">"{media.tagline}"</p>
+
       <p class="text-slate-100">{data.item.overview}</p>
-      <div class="flex gap-4">
-        <button>Does Nothing</button>
-        <button>Placeholder</button>
-      </div>
     </div>
   </div>
 </section>
