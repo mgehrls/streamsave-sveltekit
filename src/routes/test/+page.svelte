608083b9ca2d3364036968e4ebb97508c6190705
Matt Gehrls <mattgehrls@gmail.com>
2023-02-21 15:32:05 -0500

    fixed missing import

--- src/routes/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import Carousel from "$lib/Carousel/Carousel.svelte";
-  import { listItems } from "$lib/stores/listItems";
-  import type { PageData } from "./$types";
-  export let data: PageData;
-</script>
-
-<Carousel images={data.listItems.data} />
