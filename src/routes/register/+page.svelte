a11f9594d5ec26a06d44d304d75391ff824df0e2
Matt Gehrls <mattgehrls@gmail.com>
2023-10-25 17:01:37 -0400

    Revert "vite security update"
    This reverts commit f2c643c71cc7a61996a626daef3cfb5b1c30c7f3.

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -88,13 +88,11 @@
           />
           {#if password1.length > 0 && password2.length > 0 && !passwordsMatchAndValid}
             {#if password1.length < 8}
-              <p class="text-red-400 text-center transition-all">
+              <p class="text-red-400 text-center">
                 Password must be at least<br /> 8 characters
               </p>
             {:else}
-              <p class="text-red-400 text-center transition-all">
-                Passwords do not match
-              </p>
+              <p class="text-red-400 text-center">Passwords do not match</p>
             {/if}
           {/if}
         </div>
